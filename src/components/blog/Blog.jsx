40fadb2fc7bb7fb849e95fb89bc3a5049262e512
Mehraj Hosen <meherajhosen786@gmail.com>
2025-05-27 03:28:31 +0600

    map add

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,8 +1,61 @@
 import React from 'react'
+import Container from '../layouts/Container'
+import Flex from '../layouts/Flex'
+import Button from '../layouts/Button'
+import blog1 from '../../assets/blog1.png'
+import blog2 from '../../assets/blog2.png'
+import blog3 from '../../assets/blog3.png'
+import OverLay from '../layouts/OverLay'
 
 const Blog = () => {
+  const blogs= [
+    {
+      title: "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod.",
+      link: "#",
+      img: blog1
+    },
+    {
+      title: "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod.",
+      link: "#",
+      img: blog2
+    },
+    {
+      title: "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod.",
+      link: "#",
+      img: blog3
+    }
+  ]
   return (
-    <div>Blog</div>
+    <div className="blog pt-[110px] bg-light-bg pb-[162px]">
+      <div className="blogs">
+        <Container>
+          <Flex>
+            {
+              blogs.map((blog)=>(
+                <div className='box px-[45px] pt-[75px] pb-[55px] relative' style={{
+                  backgroundImage: `url(${blog.img})`,
+                  backgroundSize: "cover"
+
+                }}>
+                  <div className="relative z-10">
+                  <h3 className='w-[249px] text-white text-[24px] font-bold mb-[52px]'>{blog.title}</h3>
+                  <Button className="!bg-[rgba(255,255,255,0.5)]"><a href={blog.link}>Read More</a></Button>
+                  </div>
+                  <OverLay/>
+
+                </div>
+              ))
+            }
+
+          </Flex>
+          <div className='w-full mt-[29px] text-right'>
+            <p className='font-bold'>MORE FROM THE BLLOG</p>
+          </div>
+
+        </Container>
+      </div>
+
+    </div>
   )
 }
 
--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import map from '../../assets/map.png'
+import Container from '../layouts/Container'
+import Flex from '../layouts/Flex'
+import Button from '../layouts/Button'
 
 const Map = () => {
   return (
-    <div>Map</div>
+    <div>
+      <img src={map} alt="" />
+      <div className='py-[45px] text-white bg-primary'>
+        <Container>
+          <Flex>
+            <h2 className='text-[36px] font-bold'>Want to join as member branch in your area?</h2>
+            <Button className="border border-white text-white">Contact</Button>
+          </Flex>
+        </Container>
+
+      </div>
+    </div>
   )
 }
 
